d007d6d4c3d94525ba74429fb24d2ebd0a18c5bd
Brayan Garcia <brayangapkb@gmail.com>
2024-12-28 13:09:13 -0600

    Correción de ruta root

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
-export const RootRoute = createRootRoute({
+export const Route = createRootRoute({
   component: () => {
     return (
       <>
